360932068f2b209481085c2f1965a9d7d2b5f3b0
InsaneZeroGame <kkhlyla@gmail.com>
2018-10-08 01:55:56 +0800

    Render Graph Framework/EngineConfig.h(1) : RenderGraph Pass

---
+++ Renderer/DX12RenderGraphPass.cpp
@@ -0,0 +1,9 @@
+#include "DX12RenderGraphPass.h"
+
+Renderer::DX12RenderGraphPass::DX12RenderGraphPass()
+{
+}
+
+Renderer::DX12RenderGraphPass::~DX12RenderGraphPass()
+{
+}
---
+++ Renderer/DX12RenderGraphPass.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+namespace Renderer
+{
+    class DX12RenderGraphPass
+    {
+    public:
+        DX12RenderGraphPass();
+        ~DX12RenderGraphPass();
+
+    private:
+
+    };
+
+    
+}
